134aebe7a25c8d7d3d193b7a0886d5bf1258921f
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-06-12 03:34:45 +0000

    final tests

---
+++ build/CMakeFiles/test_ai_perf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_ai_perf.dir/test_ai_perf.cpp.o"
+  "test_ai_perf"
+  "test_ai_perf.pdb"
+  "test_ai_perf[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_ai_perf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
