31fb281033b46dfeb58cc2f28a60d8cda0c9083e
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-06-13 13:57:20 +0000

    sys works

---
+++ build/CMakeFiles/sys.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sys.dir/user_system.cpp.o"
+  "libsys.a"
+  "libsys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/xo_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xo_game.dir/main.cpp.o"
+  "xo_game"
+  "xo_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xo_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
