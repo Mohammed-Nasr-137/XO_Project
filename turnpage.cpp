bd7d40f328281447e0c31d3f18152e0da807f89c
gemyy96 <moustafa.elsayed04@eng-st.cu.edu.eg>
2025-06-18 01:46:34 +0300

    Update turnpage.cpp

--- turnpage.cpp
+++ turnpage.cpp
@@ -1,10 +1,10 @@
-#include "turnpage.h"
-#include "ui_turnpage.h"
-#include"mainscreen.h"
-#include "GameData.h"
+#include "include/turnpage.h"
 #include <QDebug>
 #include <QSettings>
-#include"gameboard.h"
+#include "include/ui_turnpage.h"
+#include"include/mainscreen.h"
+#include "include/GameData.h"
+#include"#include/gameboard.h"
 
 Turnpage::Turnpage(QWidget *parent)
     : QMainWindow(parent)
@@ -22,18 +22,17 @@ Turnpage::~Turnpage()
 void Turnpage::on_back_tomain_p_clicked()
 {
     this->hide();
-    MainScreen*mainscreen=new MainScreen();
+    MainScreen*mainscreen = new MainScreen();
     mainscreen->show();
 }
 void Turnpage::on_turn_box_currentTextChanged(const QString &arg1)
 {
     GameData::instance().P1turn = arg1;
-    if(arg1=="❌"){
-        GameData::instance().Anotherturn ="⭕" ;
+    if (arg1 == "❌"){
+        GameData::instance().Anotherturn ="⭕";
         GameData::instance().p1_move = "X";
-    }
-    else {
-        GameData::instance().Anotherturn ="❌" ;
+    }else {
+        GameData::instance().Anotherturn ="❌";
         GameData::instance().p1_move = "O";
     }
    ui->start_game->setEnabled(true);
@@ -42,7 +41,7 @@ void Turnpage::on_turn_box_currentTextChanged(const QString &arg1)
 void Turnpage::on_start_game_clicked()
 {
     this->hide();
-    Gameboard*gameboard= new Gameboard();
+    Gameboard*gameboard = new Gameboard();
     gameboard->show();
 }
 
