255a74162805ec9af9dd7090abeee46a2d9418dc
gemyy96 <moustafa.elsayed04@eng-st.cu.edu.eg>
2025-06-18 04:07:30 +0300

    Update aipage.cpp

--- aipage.cpp
+++ aipage.cpp
@@ -1,16 +1,17 @@
-#include "aipage.h"
-#include "ui_aipage.h"
+#include "include/aipage.h"
+#include "include/ui_aipage.h"
+
 #include <QDebug>
 #include <QSettings>
-#include"mainscreen.h"
-#include "GameData.h"
-#include"gameboard.h"
+
+#include "include/mainscreen.h"
+#include "include/GameData.h"
+#include "include/gameboard.h"
 #include <string>
 
 Aipage::Aipage(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Aipage)
-{
+    : QMainWindow(parent),
+      ui(new Ui::Aipage) {
     ui->setupUi(this);
     ui->level_ai->setMaxVisibleItems(3);
     ui->start_game_ai->setEnabled(false);
@@ -20,67 +21,68 @@ Aipage::Aipage(QWidget *parent)
     ui->level_ai->addItem("Medium 🧠");
     ui->level_ai->addItem("Hard 😈");
 }
-Aipage::~Aipage()
-{
+
+Aipage::~Aipage() {
     delete ui;
 }
-void Aipage::on_turn_box_ai_currentTextChanged(const QString &arg1)
-{   GameData::instance().box_2=true;
+
+void Aipage::on_turn_box_ai_currentTextChanged(const QString &arg1) {
+    GameData::instance().box_2 = true;
     GameData::instance().P1turn = arg1;
-    if(arg1=="❌"){
-        GameData::instance().Anotherturn ="⭕" ;
+    if (arg1 == "❌") {
+        GameData::instance().Anotherturn = "⭕";
         GameData::instance().p1_move = "X";
         GameData::instance().current_player = "X";
         GameData::instance().is_ai_turn = false;
-    }
-    else {
+    } else {
         qDebug() << "I'm here";
-        GameData::instance().Anotherturn ="❌" ;
+        GameData::instance().Anotherturn = "❌";
         GameData::instance().p1_move = "O";
         GameData::instance().current_player = "X";
         GameData::instance().is_ai_turn = true;
     }
-
-    if(GameData::instance().box_1 &&GameData::instance().box_2)
-    {
+    if (GameData::instance().box_1 && GameData::instance().box_2) {
         ui->start_game_ai->setEnabled(true);
     }
 }
-void Aipage::on_back_tomain_ai_clicked()
-{
+
+void Aipage::on_back_tomain_ai_clicked() {
     this->hide();
-    MainScreen*mainscreen=new MainScreen();
+    MainScreen *mainscreen = new MainScreen();
     mainscreen->show();
 }
-void Aipage::on_level_ai_currentTextChanged(const QString &arg1)
-{   GameData::instance().level= arg1;
-    GameData::instance().box_1=true;
 
-    if (arg1 == "Easy 💤") GameData::instance().ai_difficulty = "easy";
-    else if (arg1 == "Medium 🧠") GameData::instance().ai_difficulty = "medium";
-    else if (arg1 == "Hard 😈") GameData::instance().ai_difficulty = "hard";
+void Aipage::on_level_ai_currentTextChanged(const QString &arg1) {
+    GameData::instance().level = arg1;
+    GameData::instance().box_1 = true;
 
-    if(GameData::instance().box_1 &&GameData::instance().box_2)
-    {
+    if (arg1 == "Easy 💤") {
+        GameData::instance().ai_difficulty = "easy";
+    } else if (arg1 == "Medium 🧠") {
+        GameData::instance().ai_difficulty = "medium";
+    } else if (arg1 == "Hard 😈") {
+        GameData::instance().ai_difficulty = "hard";
+    }
+
+    if (GameData::instance().box_1 && GameData::instance().box_2) {
         ui->start_game_ai->setEnabled(true);
     }
 }
-void Aipage::on_start_game_ai_clicked()
-{
+
+void Aipage::on_start_game_ai_clicked() {
     this->hide();
-    GameData::instance().controller.StartNewGame(GameData::instance().firstPlayer.toStdString(), "AI", true,
-                                                 GameData::instance().ai_difficulty, GameData::instance().p1_move.toStdString());
-    Gameboard*gameboard= new Gameboard();
+    GameData::instance().controller.StartNewGame(
+        GameData::instance().firstPlayer.toStdString(),
+        "AI",
+        true,
+        GameData::instance().ai_difficulty,
+        GameData::instance().p1_move.toStdString());
+    Gameboard *gameboard = new Gameboard();
     gameboard->show();
 }
 
-
-void Aipage::on_level_ai_activated(int index)
-{
+void Aipage::on_level_ai_activated(int index) {
 }
 
-
-void Aipage::on_level_ai_currentIndexChanged(int index)
-{
+void Aipage::on_level_ai_currentIndexChanged(int index) {
 }
-
