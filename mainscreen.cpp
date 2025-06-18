68b0fc485b2a6756caafd4819fb8b451f90bc8c2
gemyy96 <moustafa.elsayed04@eng-st.cu.edu.eg>
2025-06-18 03:13:31 +0300

    Update mainscreen.cpp

--- mainscreen.cpp
+++ mainscreen.cpp
@@ -1,42 +1,41 @@
-#include "mainscreen.h"
-#include "ui_mainscreen.h"
+#include "include/mainscreen.h"
+#include "include/ui_mainscreen.h"
+
 #include <QDebug>
 #include <QSettings>
-#include "mainwindow.h"
-#include"GameData.h"
-#include"aipage.h"
-# include <QDebug>
-#include <QSettings>
+
+#include "include/mainwindow.h"
+#include "include/GameData.h"
+#include "include/aipage.h"
+
 MainScreen::MainScreen(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainScreen)
-{
+    : QMainWindow(parent),
+      ui(new Ui::MainScreen) {
     ui->setupUi(this);
 }
-MainScreen::~MainScreen()
-{
+
+MainScreen::~MainScreen() {
     delete ui;
 }
-void MainScreen::on_Logout_button_clicked()
-{
-    GameData::instance().tracker=true;
-   GameData::instance().secondPlayer="";
-   this->hide();
-    MainWindow *mainwindow=new MainWindow();
-   mainwindow->show();
-}
-void MainScreen::on_playAi_button_clicked()
-{
-     GameData::instance().mode="AI";
+
+void MainScreen::on_Logout_button_clicked() {
+    GameData::instance().tracker = true;
+    GameData::instance().secondPlayer = "";
     this->hide();
-    Aipage*aipage= new Aipage();
-    aipage->show();
-}
-void MainScreen::on_Player_button_clicked()
-{
-    GameData::instance().mode="2ndPlayer";
-    this->hide();
-    MainWindow*mainwindow=new MainWindow();
+    MainWindow *mainwindow = new MainWindow();
     mainwindow->show();
 }
 
+void MainScreen::on_playAi_button_clicked() {
+    GameData::instance().mode = "AI";
+    this->hide();
+    Aipage *aipage = new Aipage();
+    aipage->show();
+}
+
+void MainScreen::on_Player_button_clicked() {
+    GameData::instance().mode = "2ndPlayer";
+    this->hide();
+    MainWindow *mainwindow = new MainWindow();
+    mainwindow->show();
+}
