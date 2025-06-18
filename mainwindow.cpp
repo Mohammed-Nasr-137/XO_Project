4ab8bfdd1d1a087dc7ebf19c2db089e623daccb3
gemyy96 <moustafa.elsayed04@eng-st.cu.edu.eg>
2025-06-18 03:07:59 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,20 +37,20 @@ void MainWindow::on_exitgame_button_clicked() {
         if (reply == QMessageBox::Yes) {
             QApplication::quit();
         } else {
-            QMessageBox msgBox(this);
-            msgBox.setWindowTitle(" Tic Tac Toe");
-            msgBox.setTextFormat(Qt::RichText);
-            msgBox.setText(
-            "<div style='font-size:18pt; font-weight:bold;'>🎉 Enjoy playing"
-            "the game 😄🔥</div>");
-            msgBox.setIcon(QMessageBox::NoIcon);
-            QSpacerItem *spacer = new QSpacerItem(
-                400, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
-            QGridLayout *layout = reinterpret_cast<QGridLayout*>(msgBox.layout());
-            if (layout) {
-                layout->addItem(
-                    spacer, layout->rowCount(), 0, 1, layout->columnCount());
-            }
+        QMessageBox msgBox(this);
+        msgBox.setWindowTitle(" Tic Tac Toe");
+        msgBox.setTextFormat(Qt::RichText);
+        msgBox.setText(
+        "<div style='font-size:18pt; font-weight:bold;'>🎉 Enjoy playing"
+        "the game 😄🔥</div>");
+        msgBox.setIcon(QMessageBox::NoIcon);
+        QSpacerItem *spacer = new QSpacerItem(
+        400, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+        QGridLayout *layout = reinterpret_cast<QGridLayout*>(msgBox.layout());
+        if (layout) {
+            layout->addItem(
+            spacer, layout->rowCount(), 0, 1, layout->columnCount());
+                    }
             msgBox.exec();
         }
     } else {
