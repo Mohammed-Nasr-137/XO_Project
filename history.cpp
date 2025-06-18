3de93dd0a359edeb6d4e77db14d450c7ce4059ab
gemyy96 <moustafa.elsayed04@eng-st.cu.edu.eg>
2025-06-18 04:52:11 +0300

    Update history.cpp

--- history.cpp
+++ history.cpp
@@ -1,18 +1,19 @@
-#include "history.h"
-#include "ui_history.h"
-# include <QDebug>
+#include "include/history.h"
+
+#include <QDebug>
 #include <QSettings>
-#include "signup.h"
-#include"mainscreen.h"
-#include "GameData.h"
+
+#include "include/ui_history.h"
+#include "include/signup.h"
+#include "include/mainscreen.h"
+#include "include/GameData.h"
+
 History::History(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::History)
-{
+    : QMainWindow(parent),
+      ui(new Ui::History) {
     ui->setupUi(this);
 }
 
-History::~History()
-{
+History::~History() {
     delete ui;
 }
