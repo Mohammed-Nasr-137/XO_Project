e037e0602e707de5422bb15bde8c99598bba37d2
MohamedSalheen <124270603+MohamedSalheen@users.noreply.github.com>
2025-06-19 11:22:20 +0300

    Update game_wrapper.h

--- game_wrapper.h
+++ game_wrapper.h
@@ -61,5 +61,6 @@ public:
     std::tuple<int, int, int> GetHeadToHeadStats(const std::string& user1,
                                                 const std::string& user2);
     std::tuple<int, int, int> GetHumanVsAIStats(const std::string& humanUser);
+~GameWrapper();
 };
 #endif  // GAME_WRAPPER_H_
